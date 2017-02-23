388b7b72c9e155bacd3376c5496b19e1cc1be3ca
nmakwana <nmakwana@atlassoftweb.com>
2017-02-23 16:11:20 +0530

    new change in index

--- index.php
+++ index.php
@@ -2,3 +2,13 @@
 	echo "this is a php script";
 	echo "string";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1>this is home page of my project.</h1>
+</body>
+</html>
