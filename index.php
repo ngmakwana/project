49cddfe5205ef71a999cf2dcdc008b853d0bd245
nmakwana <nmakwana@atlassoftweb.com>
2017-02-23 16:58:06 +0530

    added html layout

--- index.php
+++ index.php
@@ -2,3 +2,13 @@
 	echo "this is a php script";
 	echo "temp changes in index file.";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
