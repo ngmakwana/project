4149bd548d57c70187a7d279a1868da8980aa5a8
nmakwana <nmakwana@atlassoftweb.com>
2017-02-23 16:12:54 +0530

    Revert "new change in index"
    This reverts commit 388b7b72c9e155bacd3376c5496b19e1cc1be3ca.

--- index.php
+++ index.php
@@ -2,13 +2,3 @@
 	echo "this is a php script";
 	echo "string";
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<h1>this is home page of my project.</h1>
-</body>
-</html>
